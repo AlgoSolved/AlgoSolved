ba99915fcdbba8062e49b9f69e86ba59f6edecb6
rha6780 <rha4578@gmail.com>
2023-11-12 15:54:55 +0900

    feat: project init

---
+++ src/main/java/com/example/algosolved/AlgosolvedApplication.java
@@ -0,0 +1,13 @@
+package com.example.algosolved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgosolvedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgosolvedApplication.class, args);
+	}
+
+}
