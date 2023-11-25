07f0b1ea7085162c5a6dbd9690bc536fc384ecab
rha6780 <rha4578@gmail.com>
2023-11-25 17:13:12 +0900

    fix: 잘못된 폴더 위치 수정

--- backend/src/main/java/com/example/backend/model/BaseTimeEntity.java
+++ backend/src/main/java/com/example/backend/model/BaseTimeEntity.java
@@ -1,4 +1,4 @@
-package com.example.model;
+package com.example.backend.model;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -15,7 +15,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass // 해당 맴버 변수가 컬럼이 되도록 설정
 @EntityListeners(AuditingEntityListener.class) // 변경시 자동으로 기록하도록 설정
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
 
     @CreatedDate
     @Column(name = "created_at")
--- backend/src/main/java/com/example/backend/model/Repository.java
+++ backend/src/main/java/com/example/backend/model/Repository.java
@@ -1,4 +1,4 @@
-package com.example.model;
+package com.example.backend.model;
 
 import javax.persistence.*;
 
