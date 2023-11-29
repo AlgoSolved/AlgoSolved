6ffc68123b39aff000e9ea2d90f1c587d2aabf26
youngjun0627 <yj2221@naver.com>
2023-11-29 23:33:14 +0900

    fix: primary key, serial 으로 수정 및 Problem, RequiredArgsConstructor 설정

--- backend/src/main/resources/db/migration/V1__create_problems.sql
+++ backend/src/main/resources/db/migration/V1__create_problems.sql
@@ -1,5 +1,5 @@
 CREATE TABLE problems (
-    id BIGINT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     content VARCHAR(255) NOT NULL,
     created_at timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
--- backend/src/main/resources/db/migration/V2__create_programmers_problem_details.sql
+++ backend/src/main/resources/db/migration/V2__create_programmers_problem_details.sql
@@ -1,5 +1,5 @@
 CREATE TABLE programmers_problem_details (
-    id BIGINT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     problem_id BIGINT NOT NULL,
     level INTEGER NOT NULL,
     CONSTRAINT programmers_problem_details_problem_id_fk FOREIGN KEY (problem_id) REFERENCES problems (id)
