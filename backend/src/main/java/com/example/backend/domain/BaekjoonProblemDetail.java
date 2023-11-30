89d00546142b35e728d1e4614bee977ee098fc8f
Jihyeon Park <mmmszip@gmail.com>
2023-11-30 17:38:55 +0900

    fix: BaekjoonProblemDetail 모델 수정 및 sql 파일명 변경

--- backend/src/main/java/com/example/backend/domain/BaekjoonProblemDetail.java
+++ backend/src/main/java/com/example/backend/domain/BaekjoonProblemDetail.java
@@ -1,19 +1,14 @@
 package com.example.backend.domain;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "baekjoon_problem_details")
+@Getter @Setter
 public class BaekjoonProblemDetail {
+
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
