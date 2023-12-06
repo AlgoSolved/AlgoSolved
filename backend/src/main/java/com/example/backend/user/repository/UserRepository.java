0071ff9e971b8456edfda07d7ac213ec6875b173
Jihyeon Park <mmmszip@gmail.com>
2023-12-06 17:04:05 +0900

    refactor: findByUsername 반환 타입 Optional로 변경

--- backend/src/main/java/com/example/backend/user/repository/UserRepository.java
+++ backend/src/main/java/com/example/backend/user/repository/UserRepository.java
@@ -5,6 +5,6 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  User findByUsername(String username);
+  Optional<User> findByUsername(String username);
 
 }
