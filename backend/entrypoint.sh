f9e0fe1f35b255707503a9ea477ecb1ac513b573
youngjun0627 <yj2221@naver.com>
2023-12-12 22:39:07 +0900

    feat: profile 적용

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-start_server() {
+start_dev_server() {
   (sleep 30; ./gradlew buildAndReload --continuous -PskipDownload=true -x Test) &
-  ./gradlew bootRun -PskipDownload=true
+  ./gradlew bootRun -PskipDownload=true -Dspring.profiles.active=dev
+
+
+}
+
+start_prod_server() {
+  ./gradlew bootRun -Pprofile=prod
+}
+
+start_server() {
+  echo "Starting server in $STAGE mode"
+  if [ "$STAGE" = "development" ]; then
+    start_dev_server
+  elif [ "$STAGE" = "production" ]; then
+    start_prod_server
+  else
+    echo "Invalid stage: $STAGE"
+    exit 1
+  fi
 }
 
 start_server
