55890fef2a838621cce4cbb50ecf7ee2173839f8
Jihyeon Park <mmmszip@gmail.com>
2024-01-02 23:28:48 +0900

    fix: 솔루션 빌더 추가

--- backend/src/main/java/com/example/backend/solution/repository/SolutionRepository.java
+++ backend/src/main/java/com/example/backend/solution/repository/SolutionRepository.java
@@ -1,8 +1,11 @@
 package com.example.backend.solution.repository;
 
 import com.example.backend.solution.domain.Solution;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SolutionRepository extends JpaRepository<Solution, Long> {
-
+    List<Solution> findTop10ByOrderByCreatedAtDesc();
 }
