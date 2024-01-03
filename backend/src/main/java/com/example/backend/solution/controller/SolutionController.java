b6473eb8c5858337520b279fa4d453a6904430a4
youngjun0627 <yj2221@naver.com>
2024-01-03 20:38:32 +0900

    refactor: SolutionsResponse 변환 코드 이전

--- backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
+++ backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
@@ -7,16 +7,14 @@ import com.example.backend.solution.dto.response.SolutionsResponse;
 import com.example.backend.solution.service.SolutionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -38,10 +36,8 @@ public class SolutionController {
 			allSolutions.getTotalElements()
 		);
 
-		List<SolutionResponse> solutionResponses = allSolutions.stream().map(SolutionResponse::from).toList();
-
 		return new ResponseEntity<SolutionsResponse> (
-			new SolutionsResponse(pageInfo, solutionResponses),
+			SolutionsResponse.from(pageInfo, allSolutions.getContent()),
 			HttpStatus.OK
 		);
 	}
--- backend/src/main/java/com/example/backend/solution/dto/response/SolutionsResponse.java
+++ backend/src/main/java/com/example/backend/solution/dto/response/SolutionsResponse.java
@@ -1,6 +1,7 @@
 package com.example.backend.solution.dto.response;
 
 import com.example.backend.dto.PageInfo;
+import com.example.backend.solution.domain.Solution;
 import lombok.Builder;
 
 import java.util.List;
@@ -14,5 +15,10 @@ public record SolutionsResponse (
 	public SolutionsResponse (PageInfo pageInfo, List<SolutionResponse> solutions) {
 		this.pageInfo = pageInfo;
 		this.solutions = solutions;
+	}
+
+	public static SolutionsResponse from(PageInfo pageInfo, List<Solution> solutions) {
+		List<SolutionResponse> solutionResponses = solutions.stream().map(SolutionResponse::from).toList();
+		return new SolutionsResponse(pageInfo, solutionResponses);
 	}
 }
