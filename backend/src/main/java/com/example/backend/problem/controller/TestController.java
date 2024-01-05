fbe1dc50a8b75a555c232f62692645c7d8126d39
youngjun0627 <yj2221@naver.com>
2024-01-05 21:29:59 +0900

    test: 테스트용 컨트롤러 추가

--- backend/src/main/java/com/example/backend/problem/controller/TestController.java
+++ backend/src/main/java/com/example/backend/problem/controller/TestController.java
@@ -1,32 +1,13 @@
 package com.example.backend.problem.controller;
 
-import com.example.backend.problem.domain.Problem;
-import com.example.backend.solution.domain.Solution;
-import com.example.backend.problem.repository.ProblemRepository;
-
-import com.example.backend.solution.repository.SolutionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@EnableJpaAuditing
+@RestController
 public class TestController {
-    @Autowired
-    private ProblemRepository problemRepository;
-
-    @Autowired
-    private SolutionRepository solutionRepository;
-
-//    @GetMapping("/test")
-//    public void createProblem() {
-//        Problem problem = new Problem("title", "content");
-//        problemRepository.save(problem);
-//
-//        Solution solution = new Solution();
-//        solution.setProblem(problem);
-//
-//        solutionRepository.save(solution);
-//    }
+    @GetMapping("/test")
+    public ResponseEntity<String> createProblem() {
+        return ResponseEntity.ok("test");
+    }
 }
