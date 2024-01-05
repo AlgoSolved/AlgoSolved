1a9c4b2e16d0b47f3f675797f527c0b95d24a584
Jihyeon Park <mmmszip@gmail.com>
2024-01-05 22:54:11 +0900

    refactor: 중간변수 삭제

--- backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
+++ backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
@@ -3,12 +3,9 @@ package com.example.backend.solution.controller;
 import com.example.backend.common.response.Response;
 import com.example.backend.solution.dto.SolutionDTO;
 import com.example.backend.solution.service.SolutionService;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,12 +19,9 @@ public class SolutionController {
   private final SolutionService solutionService;
 
   @GetMapping("/recent-list")
-  public ResponseEntity<Response<List<SolutionDTO>>> getRecentSolutions() {
+  public ResponseEntity<List<SolutionDTO>> getRecentSolutions() {
     List<SolutionDTO> solutionDTOList = solutionService.getRecentSolutions();
 
-    Response<List<SolutionDTO>> successResponse = Response.success(solutionDTOList);
-    return ResponseEntity.ok(successResponse);
-
-    //실패 응답
+    return new ResponseEntity(Response.success(solutionDTOList), HttpStatus.OK);
   }
 }
