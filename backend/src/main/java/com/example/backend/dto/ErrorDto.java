74216322d29227d693db2020863b3f1d2039c5c2
Jihyeon Park <mmmszip@gmail.com>
2024-01-09 23:29:48 +0900

    feat: 실패 response 생성

--- backend/src/main/java/com/example/backend/dto/ErrorDto.java
+++ backend/src/main/java/com/example/backend/dto/ErrorDto.java
@@ -1,16 +1,17 @@
 package com.example.backend.dto;
 
-import com.example.backend.common.exception.ExceptionStatus;
+import com.example.backend.common.exception.ResponseStatus;
 import lombok.Getter;
 
 
 @Getter
 public class ErrorDto {
-    private final String code;
-    private final String message;
 
-    public ErrorDto(ExceptionStatus exceptionStatus) {
-        this.code = exceptionStatus.getCode();
-        this.message = exceptionStatus.getMessage();
-    }
+  private final String code;
+  private final String message;
+
+  public ErrorDto(ResponseStatus responseStatus) {
+    this.code = responseStatus.getCode();
+    this.message = responseStatus.getMessage();
+  }
 }
