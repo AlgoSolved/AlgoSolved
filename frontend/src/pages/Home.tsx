cf357fa427ba683ce80403cf1347dd9c7da12b92
rha6780 <rha4578@gmail.com>
2024-01-21 15:03:14 +0900

    fix: Footer 및 styles 폴더 정리

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,11 +4,12 @@ import Footer from "../components/common/Footer";
 import Banner from "../components/common/Banner";
 import SolutionList from "../components/home/SolutionList";
 import useSolutionList from "../hooks/solutionList";
+import style from "../styles/pages/Home.module.css";
 
 const Home = () => {
   return (
-    <div>
-      <div>
+    <div className={style.wrapper}>
+      <div className={style.content}>
         <NavBar />
         <Banner />
         <SolutionList list={useSolutionList()} />
