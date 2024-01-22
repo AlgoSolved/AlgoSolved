7237e6b9693dff7a4a84d1781229b288d9e5dff1
Jihyeon Park <mmmszip@gmail.com>
2024-01-23 00:48:52 +0900

    refactor: 도메인별 응답구조 생성 및 관리(solution)

--- backend/src/main/java/com/example/backend/common/exception/NotFoundException.java
+++ backend/src/main/java/com/example/backend/common/exception/NotFoundException.java
@@ -1,15 +1,13 @@
 package com.example.backend.common.exception;
 
-import com.example.backend.common.response.ResponseStatus;
-
 import lombok.Getter;
 
 @Getter
 public class NotFoundException extends RuntimeException {
 
-    private ResponseStatus responseStatus;
+  private ResponseStatus responseStatus;
 
-    public NotFoundException(ResponseStatus responseStatus) {
-        this.responseStatus = responseStatus;
-    }
+  public NotFoundException(ResponseStatus responseStatus) {
+    this.responseStatus = responseStatus;
+  }
 }
--- backend/src/main/java/com/example/backend/dto/ErrorDto.java
+++ backend/src/main/java/com/example/backend/dto/ErrorDto.java
@@ -1,16 +1,15 @@
 package com.example.backend.dto;
 
-import com.example.backend.common.response.ResponseStatus;
-
 import lombok.Getter;
 
 @Getter
 public class ErrorDto {
-    private final String code;
-    private final String message;
 
-    public ErrorDto(ResponseStatus responseStatus) {
-        this.code = responseStatus.getCode();
-        this.message = responseStatus.getMessage();
-    }
+  private final String code;
+  private final String message;
+
+  public ErrorDto(ResponseStatus responseStatus) {
+    this.code = responseStatus.getCode();
+    this.message = responseStatus.getMessage();
+  }
 }
