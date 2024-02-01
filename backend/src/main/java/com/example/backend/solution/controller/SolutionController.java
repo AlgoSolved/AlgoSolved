cb2b80c50fff62a3a11f6df921485c6be26da4d9
Jihyeon Park <mmmszip@gmail.com>
2024-02-01 23:28:51 +0900

    fix: BE lint 해결

--- backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
+++ backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
@@ -4,32 +4,43 @@ import com.example.backend.common.response.BaseResponse;
 import com.example.backend.solution.dto.SolutionDTO;
 import com.example.backend.solution.response.SolutionStatus;
 import com.example.backend.solution.service.SolutionService;
-import java.util.Collections;
-import java.util.List;
+
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/solutions")
 public class SolutionController {
 
-  private final SolutionService solutionService;
+    private final SolutionService solutionService;
 
-  @GetMapping("/recent-list")
-  public ResponseEntity<List<SolutionDTO>> getRecentSolutions() {
-    List<SolutionDTO> solutionAllList = solutionService.getRecentSolutions();
+    @GetMapping("/recent-list")
+    public ResponseEntity<List<SolutionDTO>> getRecentSolutions() {
+        List<SolutionDTO> solutionAllList = solutionService.getRecentSolutions();
 
-    if (solutionAllList.isEmpty()) {
-      return new ResponseEntity(BaseResponse.success(SolutionStatus.SUCCESS_EMPTY_VALUE.getCode(),
-          SolutionStatus.SUCCESS_EMPTY_VALUE.getMessage(), Collections.emptyList()), HttpStatus.OK);
-    } else {
-      return new ResponseEntity(BaseResponse.success(SolutionStatus.SUCCESS.getCode(),
-          SolutionStatus.SUCCESS.getMessage(), solutionAllList), HttpStatus.OK);
+        if (solutionAllList.isEmpty()) {
+            return new ResponseEntity(
+                    BaseResponse.success(
+                            SolutionStatus.SUCCESS_EMPTY_VALUE.getCode(),
+                            SolutionStatus.SUCCESS_EMPTY_VALUE.getMessage(),
+                            Collections.emptyList()),
+                    HttpStatus.OK);
+        } else {
+            return new ResponseEntity(
+                    BaseResponse.success(
+                            SolutionStatus.SUCCESS.getCode(),
+                            SolutionStatus.SUCCESS.getMessage(),
+                            solutionAllList),
+                    HttpStatus.OK);
+        }
     }
-  }
 }
--- backend/src/main/java/com/example/backend/solution/response/SolutionStatus.java
+++ backend/src/main/java/com/example/backend/solution/response/SolutionStatus.java
@@ -6,13 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum SolutionStatus {
+    SUCCESS("2000", "요청에 성공하였습니다."),
+    SUCCESS_EMPTY_VALUE("2001", "최근 문제 풀이가 없습니다.");
 
-  /**
-   * 2xx : 성공
-   */
-  SUCCESS("2000", "요청에 성공하였습니다."),
-  SUCCESS_EMPTY_VALUE("2001", "최근 문제 풀이가 없습니다.");
-
-  private final String code;
-  private final String message;
+    private final String code;
+    private final String message;
 }
