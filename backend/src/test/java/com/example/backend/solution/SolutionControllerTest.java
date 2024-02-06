d8c9d98216aeae0ca4a2a4d42cad59a0c3651d14
rha6780 <rha4578@gmail.com>
2024-02-07 02:38:21 +0900

    test: 문제 풀이에 대한 API 테스트 코드 추가

--- backend/src/test/java/com/example/backend/solution/SolutionControllerTest.java
+++ backend/src/test/java/com/example/backend/solution/SolutionControllerTest.java
@@ -1,15 +1,21 @@
 package com.example.backend.solution;
 
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.backend.solution.controller.SolutionController;
 import com.example.backend.solution.dto.SolutionDTO;
+import com.example.backend.solution.dto.SolutionDetailDTO;
 import com.example.backend.solution.service.SolutionService;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.Map;
 import org.instancio.Instancio;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -18,14 +24,14 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-import java.util.Map;
-
 @WebMvcTest(SolutionController.class)
 public class SolutionControllerTest {
-    @Autowired private MockMvc mockMvc;
 
-    @MockBean private SolutionService solutionService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private SolutionService solutionService;
 
     @Test
     @WithMockUser(username = "jake", roles = "USER")
@@ -40,12 +46,12 @@ public class SolutionControllerTest {
                         content()
                                 .json(
                                         """
-                {
-                    "code": "2001",
-                    "message": "최근 문제 풀이가 없습니다.",
-                    "data": []
-                }
-                """));
+                                                {
+                                                    "code": "2001",
+                                                    "message": "최근 문제 풀이가 없습니다.",
+                                                    "data": []
+                                                }
+                                                """));
 
         // 문제 풀이가 있을 때
         SolutionDTO solutionDto = Instancio.create(SolutionDTO.class);
@@ -66,12 +72,72 @@ public class SolutionControllerTest {
                         content()
                                 .json(
                                         """
-                    {
-                        "code": "2000",
-                        "message": "요청에 성공하였습니다.",
-                        "data": %s
-                    }
-                    """
+                                                {
+                                                    "code": "2000",
+                                                    "message": "요청에 성공하였습니다.",
+                                                    "data": %s
+                                                }
+                                                """
                                                 .formatted(elements)));
+    }
+
+    @Nested
+    @DisplayName("/v1/solutions/{id} 테스트")
+    class SolutionDetailTest {
+
+        String url = "/v1/solutions/{id}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        @Test
+        @DisplayName("로그인 한 경우")
+        @WithMockUser(username = "jake", roles = "USER")
+        public void returnResponseTest() throws Exception {
+            SolutionDetailDTO solutionDetailDto = Instancio.create(SolutionDetailDTO.class);
+
+            when(solutionService.getSolution(1L)).thenReturn(solutionDetailDto);
+
+            Map<String, Object> resultMap = objectMapper.convertValue(solutionDetailDto, Map.class);
+
+            mockMvc.perform(get(url, 1L))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                    .andExpect(
+                            content()
+                                    .json(
+                                            """
+                                                    {
+                                                        "code": "2000",
+                                                        "message": "요청에 성공하였습니다.",
+                                                        "data": %s
+                                                    }
+                                                    """
+                                                    .formatted(resultMap)));
+        }
+
+        @Test
+        @DisplayName("로그인 하지 않은 경우")
+        public void invalidParamsTest() throws Exception {
+            SolutionDetailDTO solutionDetailDto = Instancio.create(SolutionDetailDTO.class);
+
+            when(solutionService.getSolution(1L)).thenReturn(solutionDetailDto);
+
+            Map<String, Object> resultMap = objectMapper.convertValue(solutionDetailDto, Map.class);
+
+            mockMvc.perform(get(url, 1L))
+                    .andExpect(status().isOk())
+                    .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                    .andExpect(
+                            content()
+                                    .json(
+                                            """
+                                                    {
+                                                        "code": "2000",
+                                                        "message": "요청에 성공하였습니다.",
+                                                        "data": %s
+                                                    }
+                                                    """
+                                                    .formatted(resultMap)));
+        }
     }
 }
