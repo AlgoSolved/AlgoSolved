065786cf9d9744dc685fb2aa8371bb6d2ce3e3da
rha6780 <rha4578@gmail.com>
2024-02-07 03:07:26 +0900

    fix: 로그인 여부에 따른 테스트 코드 수정

--- backend/src/test/java/com/example/backend/solution/SolutionControllerTest.java
+++ backend/src/test/java/com/example/backend/solution/SolutionControllerTest.java
@@ -11,7 +11,8 @@ import com.example.backend.solution.dto.SolutionDTO;
 import com.example.backend.solution.dto.SolutionDetailDTO;
 import com.example.backend.solution.service.SolutionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.util.List;
+import java.util.Map;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -23,15 +24,14 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.List;
-import java.util.Map;
-
 @WebMvcTest(SolutionController.class)
 public class SolutionControllerTest {
 
-    @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @MockBean private SolutionService solutionService;
+    @MockBean
+    private SolutionService solutionService;
 
     @Test
     @WithMockUser(username = "jake", roles = "USER")
@@ -125,19 +125,8 @@ public class SolutionControllerTest {
             Map<String, Object> resultMap = objectMapper.convertValue(solutionDetailDto, Map.class);
 
             mockMvc.perform(get(url, 1L))
-                    .andExpect(status().isOk())
-                    .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                    .andExpect(
-                            content()
-                                    .json(
-                                            """
-                                                    {
-                                                        "code": "2000",
-                                                        "message": "요청에 성공하였습니다.",
-                                                        "data": %s
-                                                    }
-                                                    """
-                                                    .formatted(resultMap)));
+                    .andExpect(status().is3xxRedirection());
+
         }
     }
 }
