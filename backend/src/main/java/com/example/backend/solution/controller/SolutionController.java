68714fa21888957f65c58ea6b0d28a56e9c0aacb
youngjun0627 <yj2221@naver.com>
2024-02-08 22:40:39 +0900

    tmp

--- backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
+++ backend/src/main/java/com/example/backend/solution/controller/SolutionController.java
@@ -1,20 +1,19 @@
 package com.example.backend.solution.controller;
 
 import com.example.backend.common.response.BaseResponse;
+import com.example.backend.lib.GithubClient;
 import com.example.backend.solution.dto.SolutionDTO;
 import com.example.backend.solution.response.SolutionStatus;
 import com.example.backend.solution.service.SolutionService;
-
+import java.util.Collections;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +21,17 @@ import java.util.List;
 public class SolutionController {
 
     private final SolutionService solutionService;
+
+    private final GithubClient githubClient;
+
+    @Value("${github.app.privateKey}")
+    private String test;
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        boolean result = githubClient.isPathExist("youngjun0627/Rgorithm");
+        return new ResponseEntity(result, HttpStatus.OK);
+    }
 
     @GetMapping("/recent-list")
     public ResponseEntity<List<SolutionDTO>> getRecentSolutions() {
