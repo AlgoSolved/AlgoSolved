482cee8a48ad7b01affeb687a247574f204daf80
Jihyeon Park <mmmszip@gmail.com>
2024-02-27 22:46:04 +0900

    feat: 유저 정보에 대한 로직 추가

---
+++ backend/src/main/java/com/example/backend/user/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.backend.user.service;
+
+import com.example.backend.common.enums.ExceptionStatus;
+import com.example.backend.common.exceptions.NotFoundException;
+import com.example.backend.github.domain.GithubRepository;
+import com.example.backend.user.domain.User;
+import com.example.backend.user.dto.UserDTO;
+import com.example.backend.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserDTO getUserByUsername(String username) {
+    User user = userRepository.findByUsername(username);
+
+    if (user == null) {
+      throw new NotFoundException(ExceptionStatus.USER_NOT_FOUND);
+    }
+
+    return UserDTO.mapToDTO(user, getSolutionCount(user.getGithubRepository()));
+  }
+
+  public Long getSolutionCount(GithubRepository githubRepository) {
+    return githubRepository.getSolutions().stream().count();
+  }
+}
