ff6f8ed746a18fa8199e3087d2b9d599c66f749a
Jihyeon Park <mmmszip@gmail.com>
2024-02-27 22:42:45 +0900

    feat: 유저 정보 controller 추가

---
+++ backend/src/main/java/com/example/backend/user/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.example.backend.user.controller;
+
+import com.example.backend.common.response.BaseResponse;
+import com.example.backend.user.dto.UserDTO;
+import com.example.backend.user.response.UserStatus;
+import com.example.backend.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/user")
+public class UserController {
+
+  private final UserService userService;
+
+  @GetMapping("/{username}")
+  public ResponseEntity<BaseResponse<UserDTO>> getUserInfo(@PathVariable String username) {
+    UserDTO userDTO = userService.getUserByUsername(username);
+
+    return new ResponseEntity(
+        BaseResponse.success(
+            UserStatus.SUCCESS.getCode(),
+            UserStatus.SUCCESS.getMessage(),
+            userDTO), HttpStatus.OK);
+  }
+}
