aae596b892f97163c09317700d6fad996f6cacf1
Jihyeon Park <mmmszip@gmail.com>
2024-02-27 22:43:18 +0900

    feat: 유저 정보 DTO 추가

---
+++ backend/src/main/java/com/example/backend/user/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.example.backend.user.dto;
+
+import com.example.backend.user.domain.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserDTO {
+
+  private String username;
+  private Long solutionCount;
+  private String githubLink;
+
+  public static UserDTO mapToDTO(User user, Long solutionCount) {
+    return UserDTO.builder()
+        .username(user.getUsername())
+        .solutionCount(solutionCount)
+        .githubLink(user.getGithubUrl())
+        .build();
+  }
+}
