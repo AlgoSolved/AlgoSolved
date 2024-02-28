549e6903aa240a655dd2c6dfad85f27fa492aa75
Jihyeon Park <mmmszip@gmail.com>
2024-02-28 21:45:29 +0900

    feat: 유저 탈퇴 서비스 추가

--- backend/src/main/java/com/example/backend/user/service/UserService.java
+++ backend/src/main/java/com/example/backend/user/service/UserService.java
@@ -6,6 +6,7 @@ import com.example.backend.github.domain.GithubRepository;
 import com.example.backend.user.domain.User;
 import com.example.backend.user.dto.UserDTO;
 import com.example.backend.user.repository.UserRepository;
+import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +22,21 @@ public class UserService {
     if (user == null) {
       throw new NotFoundException(ExceptionStatus.USER_NOT_FOUND);
     }
-
     return UserDTO.mapToDTO(user, getSolutionCount(user.getGithubRepository()));
   }
 
   public Long getSolutionCount(GithubRepository githubRepository) {
     return githubRepository.getSolutions().stream().count();
   }
+
+  @Transactional
+  public boolean deleteUser(String username) {
+    User user = userRepository.findByUsername(username);
+    if (user != null) {
+      userRepository.delete(user);
+      return true;
+    }
+    return false;
+  }
 }
+
