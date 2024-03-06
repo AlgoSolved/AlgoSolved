c8313dad48af4534e140352d4afbb75b976fa55a
Jihyeon Park <mmmszip@gmail.com>
2024-03-07 04:53:35 +0900

    test: 회원 탈퇴 테스트 추가

--- backend/src/test/java/com/example/backend/user/UserControllerTest.java
+++ backend/src/test/java/com/example/backend/user/UserControllerTest.java
@@ -1,6 +1,7 @@
 package com.example.backend.user;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,9 +33,9 @@ public class UserControllerTest {
   private UserService userService;
 
   @Test
-  @DisplayName("v1/users/{id} 성공 테스트")
+  @DisplayName("id에 해당하는 유저가 존재하는 경우 관련된 정보를 반환한다.")
   @WithMockUser(username = "jake", roles = "USER")
-  void 유저_정보_성공() throws Exception {
+  void 유저_정보_반환_성공() throws Exception {
     UserDTO.Profile userProfile = Instancio.create(UserDTO.Profile.class);
     when(userService.getUserProfile(1L)).thenReturn(userProfile);
 
@@ -58,9 +59,9 @@ public class UserControllerTest {
   }
 
   @Test
-  @DisplayName("v1/users/{id} 실패 테스트")
+  @DisplayName("id에 해당하는 유저가 존재하지 않은 경우 에러를 반환한다.")
   @WithMockUser(username = "jake", roles = "USER")
-  void 유저_정보_실패() throws Exception {
+  void 유저_정보_반환_실패() throws Exception {
     when(userService.getUserProfile(1L))
         .thenThrow(new NotFoundException(ExceptionStatus.NOT_FOUND));
 
@@ -68,5 +69,26 @@ public class UserControllerTest {
             get("/v1/users/{1}", 1)
                 .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNotFound());
+  }
+
+  @Test
+  @DisplayName("사용자가 입력한 유저네임이 일치한 경우 true를 반환한다.")
+  @WithMockUser(username = "jake", roles = "USER")
+  void 유저_탈퇴_성공() throws Exception {
+    mockMvc.perform(delete("/v1/user/{id}", 1)
+            .param("inputUsername", "jake"))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(
+            content()
+                .json(
+                    """
+                        {
+                            "code": "2000",
+                            "message": "요청에 성공하였습니다."
+                        }
+                        """
+                ));
+
   }
 }
