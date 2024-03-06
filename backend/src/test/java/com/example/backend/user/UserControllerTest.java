b451d245f65714e7a8f974b9966476babde173aa
Jihyeon Park <mmmszip@gmail.com>
2024-03-07 03:47:43 +0900

    test: 유저컨트롤러 테스트 깨짐 수정

--- backend/src/test/java/com/example/backend/user/UserControllerTest.java
+++ backend/src/test/java/com/example/backend/user/UserControllerTest.java
@@ -11,7 +11,7 @@ import com.example.backend.user.controller.UserController;
 import com.example.backend.user.dto.UserDTO;
 import com.example.backend.user.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
+import java.util.Map;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,53 +22,51 @@ import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.Map;
-
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
 
-    @Autowired private MockMvc mockMvc;
+  @Autowired
+  private MockMvc mockMvc;
 
-    @MockBean private UserService userService;
+  @MockBean
+  private UserService userService;
 
-    @Test
-    @DisplayName("v1/users/{username} 성공 테스트")
-    @WithMockUser(username = "jake", roles = "USER")
-    void 유저_정보_성공() throws Exception {
-        UserDTO userDTO = Instancio.create(UserDTO.class);
-        when(userService.getUserByUsername("jake")).thenReturn(userDTO);
+  @Test
+  @DisplayName("v1/users/{id} 성공 테스트")
+  @WithMockUser(username = "jake", roles = "USER")
+  void 유저_정보_성공() throws Exception {
+    UserDTO.Profile userProfile = Instancio.create(UserDTO.Profile.class);
+    when(userService.getUserProfile(1L)).thenReturn(userProfile);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> resultMap = objectMapper.convertValue(userDTO, Map.class);
+    ObjectMapper objectMapper = new ObjectMapper();
+    Map<String, Object> resultMap = objectMapper.convertValue(userProfile, Map.class);
 
-        mockMvc.perform(get("/v1/user/{username}", "jake"))
-                .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(
-                        content()
-                                .json(
-                                        """
+    mockMvc.perform(get("/v1/user/{id}", 1))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(
+            content()
+                .json(
+                    """
                         {
                             "code": "2000",
                             "message": "요청에 성공하였습니다.",
                             "data": %s
                         }
                         """
-                                                .formatted(resultMap)));
-    }
+                        .formatted(resultMap)));
+  }
 
-    @Test
-    @DisplayName("v1/users/{username} 실패 테스트")
-    @WithMockUser(username = "nonexistentuser", roles = "USER")
-    void 유저_정보_실패() throws Exception {
+  @Test
+  @DisplayName("v1/users/{id} 실패 테스트")
+  @WithMockUser(username = "jake", roles = "USER")
+  void 유저_정보_실패() throws Exception {
+    when(userService.getUserProfile(1L))
+        .thenThrow(new NotFoundException(ExceptionStatus.NOT_FOUND));
 
-        String nonexistentUsername = "nonexistentuser";
-        when(userService.getUserByUsername(nonexistentUsername))
-                .thenThrow(new NotFoundException(ExceptionStatus.USER_NOT_FOUND));
-
-        mockMvc.perform(
-                        get("/v1/users/{username}", nonexistentUsername)
-                                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isNotFound());
-    }
+    mockMvc.perform(
+            get("/v1/users/{1}", 1)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
+  }
 }
