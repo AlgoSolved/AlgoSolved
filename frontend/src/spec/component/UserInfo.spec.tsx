4d9c2c3b83c0e5e91429f74deb5e1634f073aa0e
rha6780 <rha4578@gmail.com>
2024-03-07 21:30:44 +0900

    fix: import group 관련 lint 수정

--- frontend/src/spec/component/UserInfo.spec.tsx
+++ frontend/src/spec/component/UserInfo.spec.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { render, renderHook, waitFor } from "@testing-library/react";
+
 import "@testing-library/jest-dom/extend-expect";
 
 import { getUserInfo } from "../../apis/v1/users/info";
