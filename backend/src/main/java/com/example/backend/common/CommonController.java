d7806596be3e96d199cc4f21f828dbe6037654c6
rha6780 <rha4578@gmail.com>
2024-03-07 21:29:35 +0900

    fix: 안맞는 lint 수정

--- backend/src/main/java/com/example/backend/common/CommonController.java
+++ backend/src/main/java/com/example/backend/common/CommonController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CommonController {
-    
+
     @GetMapping("/health/ping")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("pong");
