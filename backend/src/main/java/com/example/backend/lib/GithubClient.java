f129c73ff993f91323bd5d18990f92b5b445d71a
rha6780 <rha4578@gmail.com>
2024-03-11 21:50:26 +0900

    fix: getFileContent 시 길이 제한에 맞는 문제만 저장하도록 수정

--- backend/src/main/java/com/example/backend/lib/GithubClient.java
+++ backend/src/main/java/com/example/backend/lib/GithubClient.java
@@ -2,8 +2,11 @@ package com.example.backend.lib;
 
 import com.example.backend.util.JWTGenerator;
 
+import lombok.extern.slf4j.Slf4j;
+
 import org.kohsuke.github.GHAppInstallation;
 import org.kohsuke.github.GHAppInstallationToken;
+import org.kohsuke.github.GHContent;
 import org.kohsuke.github.GHTreeEntry;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
@@ -12,8 +15,12 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Slf4j
 @Component
 public class GithubClient {
+
+    private final long ttlMillis = 600000;
+
     @Value("${github.app.id}")
     private String githubAppId;
 
@@ -23,7 +30,8 @@ public class GithubClient {
     @Value("${github.app.installationId}")
     private Long githubAppInstallationId;
 
-    private final long ttlMillis = 600000;
+    @Value("${github.app.maxFileSize}")
+    private Long githubAppMaxFileSize;
 
     public Boolean isPathExist(String repo) {
         try {
@@ -63,7 +71,12 @@ public class GithubClient {
     public String getContent(String repo, String path) {
         try {
             GitHub github = buildGithub();
-            return github.getRepository(repo).getFileContent(path).getContent();
+            GHContent content = github.getRepository(repo).getFileContent(path);
+            if (content.getSize() > githubAppMaxFileSize) {
+                log.error("{} file size is over max file size", path);
+                return null;
+            }
+            return content.getContent();
         } catch (Exception e) {
             e.printStackTrace();
             return null;
