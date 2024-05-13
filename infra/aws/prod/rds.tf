101b24cafa2d20e67e035e1f7ca0e65fe5bfe1bd
youngjun0627 <yj2221@naver.com>
2024-05-13 11:45:25 +0900

    fmt 적용

--- infra/aws/prod/rds.tf
+++ infra/aws/prod/rds.tf
@@ -4,18 +4,18 @@ resource "random_string" "password" {
 }
 
 resource "aws_db_instance" "algosolved-rdb" {
-  depends_on             = [
+  depends_on = [
     aws_security_group.algosolved-rds-sg,
     aws_db_subnet_group.algosovled-subnet-group
   ]
 
-  db_name           = "${var.service}-rdb"
-  engine            = "postgres"
-  engine_version    = "16"
-  instance_class    = "db.t2.micro"
-  allocated_storage = 10
+  db_name                = "${var.service}-rdb"
+  engine                 = "postgres"
+  engine_version         = "16"
+  instance_class         = "db.t2.micro"
+  allocated_storage      = 10
   vpc_security_group_ids = [aws_security_group.algosolved-rds-sg.id]
-  db_subnet_group_name = aws_db_subnet_group.algosovled-subnet-group.name
+  db_subnet_group_name   = aws_db_subnet_group.algosovled-subnet-group.name
 
   username = "root"
   password = random_string.password.result
@@ -27,8 +27,8 @@ resource "aws_db_instance" "algosolved-rdb" {
 }
 
 resource "aws_db_subnet_group" "algosovled-subnet-group" {
-  name        = "${var.service}-subnet-group"
-  subnet_ids  = [var.sub_pub_a_id, var.sub_pub_b_id, var.sub_pub_c_id, var.sub_pub_d_id]
+  name       = "${var.service}-subnet-group"
+  subnet_ids = [var.sub_pub_a_id, var.sub_pub_b_id, var.sub_pub_c_id, var.sub_pub_d_id]
 
   tags = {
     Project = var.project
