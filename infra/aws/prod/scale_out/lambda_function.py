c3fed163da2ece2485ce037f2677cc30596e2433
mmmtobezip <mmmszip@gmail.com>
2024-05-22 01:56:26 +0900

    fix: scale_out lambda function rds start 추가

--- infra/aws/prod/scale_out/lambda_function.py
+++ infra/aws/prod/scale_out/lambda_function.py
@@ -2,27 +2,45 @@ import json
 import boto3
 
 asg = boto3.client('autoscaling')
+rds = boto3.client('rds')
 
 def lambda_handler(event, context):
   desired_capacity = event['detail']['DesiredCapacity']
   auto_scaling_group_name = event['detail']['AutoScalingGroupName']
 
-  print(f"Auto Scaling Group Name: {auto_scaling_group_name}")
-  print(f"Desired Capacity: {desired_capacity}")
+  rds_instances = event['resources'][0]
+  action = event['detail']['Action']
 
-  update_autoscaling_group(auto_scaling_group_name, desired_capacity)
+  print(f"Event details:\n"
+        f"  Auto Scaling Group: {auto_scaling_group_name}\n"
+        f"  Desired Capacity: {desired_capacity}\n"
+        f"  RDS Instance Identifier: {rds_instances}\n"
+        f"  Action: {action}")
+
+  if action == 'start':
+    start_rds_instances(rds_instances)
+    update_autoscaling_group(auto_scaling_group_name, desired_capacity)
 
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello from Lambda!')
+    'body': json.dumps('Success: Server count 1 and ASG count 1')
   }
+
+def start_rds_instances(rds_instances):
+  try:
+    rds.start_db_instances(DBInstanceIdentifier=rds_instances)
+    print(f"Successfully started RDS instance: {rds_instances}")
+  except Exception as e:
+    print(e)
 
 def update_autoscaling_group(auto_scaling_group_name, desired_capacity):
   try:
-    asg_response = asg.update_autoscaling_group(
+    asg.update_autoscaling_group(
         AutoScalingGroupName = auto_scaling_group_name,
         DesiredCapacity = desired_capacity
     )
-    print(f"Succesed Update DesiredCapacity to {auto_scaling_group_name}")
+    print(f"Successfully updated ASG DesiredCapacity: {desired_capacity}")
   except Exception as e:
     print(e)
+
+
