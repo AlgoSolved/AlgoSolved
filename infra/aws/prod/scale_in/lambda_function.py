361e14bcb6c89999487ae7e236356f693246f69f
mmmtobezip <mmmszip@gmail.com>
2024-05-22 01:55:51 +0900

    fix: scale_in lambda function 코드 수정

--- infra/aws/prod/scale_in/lambda_function.py
+++ infra/aws/prod/scale_in/lambda_function.py
@@ -8,13 +8,14 @@ def lambda_handler(event, context):
   desired_capacity = event['detail']['DesiredCapacity']
   auto_scaling_group_name = event['detail']['AutoScalingGroupName']
 
-  rds_instances = event['resources'][0].split(':')[-1]
+  rds_instances = event['resources'][0]
   action = event['detail']['Action']
 
-  print(f"Auto Scaling Group Name: {auto_scaling_group_name}")
-  print(f"Desired Capacity: {desired_capacity}")
-  print(f"RDS Instances: {rds_instances}")
-  print(f"Action: {action}")
+  print(f"Event details:\n"
+        f"  Auto Scaling Group: {auto_scaling_group_name}\n"
+        f"  Desired Capacity: {desired_capacity}\n"
+        f"  RDS Instance Identifier: {rds_instances}\n"
+        f"  Action: {action}")
 
   if action == 'stop':
     stop_rds_instances(rds_instances)
@@ -22,23 +23,23 @@ def lambda_handler(event, context):
 
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello from Lambda!')
+    'body': json.dumps('Success: Server count 0 and ASG desired capacity 0')
   }
 
 def stop_rds_instances(rds_instances):
   try:
-    rds.stop_rds_instances(DBInstanceIdentifier=rds_instances)
-    print(f"Success: stop_rds_instances {rds_instances}")
+    rds.stop_db_instances(DBInstanceIdentifier=rds_instances)
+    print(f"Successfully started RDS instance:{rds_instances}")
   except Exception as e:
     print(e)
 
 
 def update_autoscaling_group(auto_scaling_group_name, desired_capacity):
   try:
-    asg_response = asg.update_autoscaling_group(
+    asg.update_autoscaling_group(
         AutoScalingGroupName = auto_scaling_group_name,
         DesiredCapacity = desired_capacity
     )
-    print(f"Succesed Update DesiredCapacity to {auto_scaling_group_name}")
+    print(f"Successfully updated ASG DesiredCapacity: {desired_capacity}")
   except Exception as e:
     print(e)
