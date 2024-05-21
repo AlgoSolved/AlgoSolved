51ec352b57e3b589cc55c4ef93303861163b7055
mmmtobezip <mmmszip@gmail.com>
2024-05-21 17:54:48 +0900

    fix: policy json으로 변경

--- infra/aws/prod/iam.tf
+++ infra/aws/prod/iam.tf
@@ -1,50 +1,56 @@
+# Lambda iam
 resource "aws_iam_role" "lambda_role" {
   name               = "algosolved-lambda-role"
-  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
-}
-
-data "aws_iam_policy_document" "lambda_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      identifiers = ["lambda.amazonaws.com"]
-      type        = "Service"
-    }
-  }
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action": [
+          "sts:AssumeRole"
+        ],
+        "Principal": {
+          "Service": "lambda.amazonaws.com"
+        },
+        "Effect": "Allow"
+      }
+    ]
+  })
 }
 
 # RDS
 resource "aws_iam_policy" "rds_management_policy" {
   name        = "rds-management-policy"
   description = "Policy to manage RDS instances"
-  policy      = data.aws_iam_policy_document.rds_management_policy_doc.json
-}
-
-data "aws_iam_policy_document" "rds_management_policy_doc" {
-  statement {
-    actions = [
-      "rds: StartDBInstance",
-      "rds: StopDBInstance",
-      "rds:DescribeDBInstances"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "rds: StartDBInstance",
+          "rds: StopDBInstance",
+          "rds: DescribeDBInstances"
+        ],
+        "Resource" : "*"
+      }
     ]
-    resources = ["*"]
-  }
+  })
 }
 
 # AutoScaling Group
 resource "aws_iam_policy" "asg_management_policy" {
   name        = "autoscaling-management-policy"
   description = "Policy to manage AutoScaling groups"
-  policy      = data.aws_iam_policy_document.asg_management_policy_doc.json
-}
-
-data "aws_iam_policy_document" "asg_management_policy_doc" {
-  statement {
-    actions = [
-      "autoscaling:UpdateAutoScalingGroup"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "autoscaling: UpdateAutoScalingGroup"
+        ],
+        "Resource" : "*"
+      }
     ]
-    resources = ["*"]
-  }
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "asg_policy_attachment" {
