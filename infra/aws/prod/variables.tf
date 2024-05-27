8fc2ee18efca0687e83807d4141eb52d6f8dfe56
rha6780 <rha4578@gmail.com>
2024-05-28 00:21:40 +0900

    fix: 특정 variables의 sensitive 설정 추가

--- infra/aws/prod/variables.tf
+++ infra/aws/prod/variables.tf
@@ -12,30 +12,39 @@ variable "stage" {
 ## vpc
 variable "vpc_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pub_a_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pub_b_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pub_c_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pub_d_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pri_a_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pri_b_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pri_c_id" {
   type = string
+  sensitive = true
 }
 variable "sub_pri_d_id" {
   type = string
+  sensitive = true
 }
 
 
@@ -45,4 +54,5 @@ variable "ec2_instance_type" {
 }
 variable "ec2_key" {
   type = string
+  sensitive = true
 }
