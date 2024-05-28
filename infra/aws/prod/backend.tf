e7ffa7a3db5101e4919db9cfe282fb58a5141357
youngjun0627 <yj2221@naver.com>
2024-05-28 18:02:55 +0900

    refactor: lint formatting

--- infra/aws/prod/backend.tf
+++ infra/aws/prod/backend.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = "1.8.1"
 
   backend "s3" {
-    bucket = "algosolved-terraform-s3"
-    key    = "infra/prod/terraform.tfstate"
-    region = "ap-northeast-2"
+    bucket         = "algosolved-terraform-s3"
+    key            = "infra/prod/terraform.tfstate"
+    region         = "ap-northeast-2"
     dynamodb_table = "algosolved-terraform-lock"
   }
 }
