e877ae6f8c870b01a7c3928b9b93f1de3596ba1f
youngjun0627 <yj2221@naver.com>
2024-05-30 19:47:55 +0900

    임시 주석처리

--- infra/aws/prod/route53.tf
+++ infra/aws/prod/route53.tf
@@ -1,15 +1,15 @@
-resource "aws_route53_zone" "public_zone" {
-  name = "algosolved.com"
-}
-
-resource "aws_route53_record" "algosolved_record" {
-  zone_id = aws_route53_zone.public_zone.zone_id
-  name    = "algosolved.com"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.algosolved-lb.dns_name
-    zone_id                = aws_lb.algosolved-lb.zone_id
-    evaluate_target_health = true
-  }
-}
+#resource "aws_route53_zone" "public_zone" {
+#  name = "algosolved.com"
+#}
+#
+#resource "aws_route53_record" "algosolved_record" {
+#  zone_id = aws_route53_zone.public_zone.zone_id
+#  name    = "algosolved.com"
+#  type    = "A"
+#
+#  alias {
+#    name                   = aws_lb.algosolved-lb.dns_name
+#    zone_id                = aws_lb.algosolved-lb.zone_id
+#    evaluate_target_health = true
+#  }
+#}
