6650b093f6a84e504a7e2eef8aab9f0ac71f20cd
rha6780 <rha4578@gmail.com>
2024-06-23 22:32:46 +0900

    fix: front 빌드 시 warning 을 패스하도록 수정

--- infra/aws/prod/elb.tf
+++ infra/aws/prod/elb.tf
@@ -83,32 +83,31 @@ resource "aws_alb_listener" "algosolved-http-forward" {
   }
 }
 
-// TODO : HTTPS 를 지원하기 위해 인증서가 필요함(domain-CRA)
-# resource "aws_acm_certificate" "algosolved-cert" {
-#   domain_name = "example.com"
-#   validation_method = "DNS"
-# }
+ resource "aws_acm_certificate" "algosolved-cert" {
+   domain_name = "algosolved.ord"
+   validation_method = "DNS"
+ }
 
-# resource "aws_alb_listener" "algosolved-https-listener" {
-#   load_balancer_arn = aws_lb.algosolved-lb.arn
-#   port              = 443
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+ resource "aws_alb_listener" "algosolved-https-listener" {
+   load_balancer_arn = aws_lb.algosolved-lb.arn
+   port              = 443
+   protocol          = "HTTPS"
+   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
 
-#   default_action {
-#     target_group_arn = aws_lb_target_group.algosolved-lb-tg.arn
-#     type             = "forward"
-#   }
+   default_action {
+     target_group_arn = aws_lb_target_group.algosolved-lb-tg.arn
+     type             = "forward"
+   }
 
-#   timeouts {}
+   timeouts {}
 
-#   tags = {
-#     Project = var.project
-#     Stage   = var.stage
-#   }
-#   tags_all = {
-#     Project = var.project
-#     Stage   = var.stage
-#   }
-# }
+   tags = {
+     Project = var.project
+     Stage   = var.stage
+   }
+   tags_all = {
+     Project = var.project
+     Stage   = var.stage
+   }
+ }
 
--- infra/aws/prod/route53.tf
+++ infra/aws/prod/route53.tf
@@ -13,3 +13,36 @@ resource "aws_route53_record" "algosolved_record" {
     evaluate_target_health = true
   }
 }
+
+
+resource "tls_private_key" "algosolved-tls-key" {
+  algorithm = "ECDSA"
+}
+
+resource "tls_self_signed_cert" "algosolved-tls-cert" {
+  key_algorithm   = tls_private_key.algosolved-tls-key.algorithm
+  private_key_pem = tls_private_key.algosolved-tls-key.private_key_pem
+
+  validity_period_hours = 12
+  early_renewal_hours = 3
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+  ]
+
+  dns_names = ["algosolved.org"]
+
+  subject {
+    common_name  = "algosolved.org"
+    organization = "ACME Examples, Inc"
+  }
+}
+
+resource "aws_iam_server_certificate" "algosolved-cert" {
+  name             = "algosolved_cert"
+  certificate_body = tls_self_signed_cert.algosolved-tls-cert.cert_pem
+  private_key      = tls_private_key.algosolved-tls-key.private_key_pem
+}
+
